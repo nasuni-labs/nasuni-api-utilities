250403daa69765004d1b8c46b1ea6c0e130f07a9
Coan Dillahunty <90055426+cdillahunty-nasuni@users.noreply.github.com>
2022-10-07 10:39:11 -0500

    Update SetGFLandMode.ps1
    Added retries for setting global lock to avoid contention with snapshots in progress

--- Paths/SetGFLandMode.ps1
+++ Paths/SetGFLandMode.ps1
@@ -1,39 +1,37 @@
-#Sets GFL Snapshot Status for a path
-#Checks the Volumes snapshot status for all Edge Appliances and waiting until idle before executing
-#It then checks to see if the path is valid before setting GFL and mode.
-
-#Uses the list snapshot statuses for a volume NMC API endpoint
-#http://docs.api.nasuni.com/nmc/api/1.1.0/index.html#list-snapshot-statuses-for-a-volume
-
-#populate NMC hostname and credentials
-$hostname = "insertNMChostnameHere"
+<# Sets GFL Snapshot Status for the path provided and retries using a delay until complete
+Does not check snapshot status before attempting to set GFL
+Checks the message status after submitting GFL request to see if the operation completed successfully
+Writes the following info to the console: path, retry number, status
+Includes error handling for invalid paths and has a progress counter #>
   
-#username for AD accounts supports both UPN (user@domain.com) and DOMAIN\\samaccountname formats (two backslashes required ). Nasuni Native user accounts are also supported.
+#populate NMC hostname and credentials
+$hostname = "InsertNMCHostname"
+  
+#username for AD accounts supports both UPN (user@domain.com) and DOMAIN\\samaccountname formats (two backslashes required).
+#Nasuni Native user accounts are also supported.
 $username = "username"
 $password = 'password'
-$credentials = '{"username":"' + $username + '","password":"' + $password + '"}'
-  
-#specify Nasuni volume guid
-$volume_guid = "InsertVolumeGuid"
+ 
+#specify Volume GUID
+$volume_guid = "insertVolumeGUID"
 
-#Set the path for GetPathInfo. The path should start with a "\", is case sensitive, and is the path as displayed in the NMC file browser
-# and is not related to the share path.
-$FolderPath = "\insert\path\here" 
+#specify folder path using slashes (/) - do not include a trailing slash
+$path = "/folder1/folder2"
 
 #Set the desired GFL mode - "optimized, advanced, or asynchronous"
 $mode = "optimized"
 
-#Specify Number of times to Retry before giving up
-$RetryLimit = "10"
+#Specify Number of times to retry before giving up
+$RetryLimit = 10
 
 #Specify delay between retries in seconds
-$RetryDelay = "30"
+$RetryDelay = 30
 
 #end variables
 
-#change the direction of slashes in folder path for use with the NMC API
-$FolderPath = $FolderPath -replace '\\', '/'
-
+#combine credentials for NMC authentication
+$credentials = '{"username":"' + $username + '","password":"' + $password + '"}'
+  
 #Request token and build connection headers
 # Allow untrusted SSL certs
 if ($PSVersionTable.PSEdition -eq 'Core') #PowerShell Core
@@ -62,108 +60,93 @@ public class TrustAllCertsPolicy : ICertificatePolicy {
 #set the correct TLS Type
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
  } }
-
+ 
 #build JSON headers
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Accept", 'application/json')
 $headers.Add("Content-Type", 'application/json')
   
-#construct Uri for the token
+#construct Uri for login
 $url="https://"+$hostname+"/api/v1.1/auth/login/"
-   
+  
 #Use credentials to request and store a session token from NMC for later use
 $result = Invoke-RestMethod -Uri $url -Method Post -Headers $headers -Body $credentials
 $token = $result.token
 $headers.Add("Authorization","Token " + $token)
 
-#Get Volume info URL to find the owning Edge Appliance Serial Number
-$VolumeInfoURL="https://"+$hostname+"/api/v1.1/volumes/" + $volume_guid + "/filers/"
+#start working on setting GFL
 
-$GetVolumeInfo = Invoke-RestMethod -Uri $VolumeInfoURL -Method Get -Headers $headers
-$filer_serial = $GetVolumeInfo.items[0].filer_serial_number
-
-#Get snapshot status URL
-$SnapshotStatusURL="https://"+$hostname+"/api/v1.1/volumes/" + $volume_guid + "/filers/snapshots/"
-
-#Set the retry counter to one before starting the loop
+#set the RetryCount to 1 before begging the loop
 $RetryCount = 1
+
+write-output "Setting $mode GFL mode on: $path"
 
 #run loop the number of times specified by the Retry Limit or until the snapshot status is idle
 DO
 {
-write-output "Starting Loop $RetryCount"
-#check for Volume snapshot status for all Edge Appliances
-$GetSnapshotInfo = Invoke-RestMethod -Uri $SnapshotStatusURL -Method Get -Headers $headers
-    foreach($i in 0..($GetSnapshotInfo.items.Count-1)){
-        $SnapshotArrayStatus = $GetSnapshotInfo.items[$i].snapshot_status
-        $SnapshotArrayFiler = $GetSnapshotInfo.items[$i].filer_serial_number
-        write-host $SnapshotArrayStatus "for filer serial number" $SnapshotArrayFiler
-            if ($SnapshotArrayStatus -ne "idle") {
-            $SnapshotBusy = $true
-            write-output "Setting Snapshot Busy to True"
-            }
-    $i++
-    }
+write-output "Loop $RetryCount"
 
-#Execute command if Snapshot status is idle, otherwise retry
-    if ($SnapshotBusy -ne $true) {
-        write-output "Snapshot is Idle for all Edge Appliances running set GFL command"
-        #Build the URL for the endpoints
-        $PathInfoURL="https://"+$hostname+"/api/v1.1/volumes/" + $volume_guid + "/filers/" + "$filer_serial" + "/path" + $FolderPath
- 
-        #Refresh Stats on the supplied path
-        $RefreshPathInfo=Invoke-RestMethod -Uri $PathInfoURL -Method POST -Headers $headers
+#enable GFL for path
+#Set the URL for the folder update NMC API endpoint
+$GFLurl="https://"+$hostname+"/api/v1.1/volumes/" + $volume_guid + "/global-lock-folders/"
+            
+#build the body for the folder update
+$body = @{
+path = $Path
+mode = $mode
+}
 
-        #sleep to allow time for the refresh to complete
-        Start-Sleep -s 1
+#set GFL and mode for the specified path
+$SetGFL=Invoke-RestMethod -Uri $GFLurl -Method Post -Headers $headers -Body (ConvertTo-Json -InputObject $body)
 
-        #Lookup the status of the message ID that is returned from the NMC when POSTing to the Refresh Stats NMC API endpoint
-        $Message=Invoke-RestMethod -Uri $RefreshPathInfo.message.links.self.href -Method Get -Headers $headers
+#wait for setting GFL to complete
+start-sleep -Seconds 5
 
-            #check for the pending condition and if pending, retry after 5 seconds
-            if ($Message.status -eq "pending") {
-            start-sleep -s 5
-            $Message=Invoke-RestMethod -Uri $RefreshPathInfo.message.links.self.href -Method Get -Headers $headers
-            }
+#see what happened when setting GFL
+$Message=Invoke-RestMethod -Uri $SetGFL.message.links.self.href -Method Get -Headers $headers
 
-        #Check to see if the path is valid by checking to see if the status returns as synced. If it does, set GFL for the path. 
-            #If the the status is something else, skip and log.
-            if ($Message.status -eq "synced") {
-            #enable GFL for path
-            #Set the URL for the folder update NMC API endpoint
-            $GFLurl="https://"+$hostname+"/api/v1.1/volumes/" + $volume_guid + "/global-lock-folders/"
-
-            #build the body for the folder update
-            $body = @{
-            path = $FolderPath
-            mode = $mode
-            }
-            $SetGFL=Invoke-RestMethod -Uri $GFLurl -Method Post -Headers $headers -Body (ConvertTo-Json -InputObject $body)
-            write-output $SetGFL | ConvertTo-Json
-            }
-            else {
-    #skip and log pending messages or failure
-    if ($Message.status -eq "pending") {write-output "Refresh Path Info operation still pending--please try again"}
-    else {write-output $message.status}
-            }
+    #check for the pending condition and if pending, retry after 5 seconds
+    if ($Message.status -eq "pending") {
+        start-sleep -s 10
+        #get the message again to see if waiting longer helped
+        $Message=Invoke-RestMethod -Uri $SetGFL.message.links.self.href -Method Get -Headers $headers
+        }
+    #if message status is synced the set GFL operation was successful
+    if ($Message.status -eq "synced") {
+        $status = "success"
+        #this path is done - exit the loop
+        write-output $status
+        break
+        }
+    #handle failure cases
+    if ($Message.status -eq "failure") {
+        if ($Message.error.description -like "*snapshot is taking place*"){
+            #snapshot is busy - we need to retry
+            write-output "Snapshot Busy"
+        }
+        else {
+            #failure from invalid path -log and exit loop
+            $status = "Invalid Path"
+            write-output $status    
             break
-            } else {
-            #sleep for defined time in Retry delay
-            write-host("sleeping for $RetryDelay seconds")
-            Start-Sleep -s $RetryDelay
-            #end setGFL condition
-            }
+        }
+        #clean up the sync error
+        $DeleteMessageURL = $message.links.acknowledge.href
+        #call deleteMessage uses a variable to suppress output
+        $DeleteMessage = Invoke-RestMethod -Uri $DeleteMessageURL -Method Delete -Headers $headers -Body $credentials
+        }
 
-#Increment the RetryCount and clear SnapshotBusy before retrying
+ #Increment the RetryCount before retrying
  $RetryCount++
- Clear-Variable SnapshotBusy
 
- write-output "Now `$Retries is $RetryCount"
  #If the end of the loop completes and the snapshot is still running, log the results
- if ($RetryCount -eq $RetryLimit) {
+ if ($RetryCount -ge $RetryLimit) {
      write-output "Snapshot still running--retries exceeded"
+     $status = "retryExceeded"
  }
 
-} Until ($RetryCount -eq $RetryLimit)
+#sleep for defined time in Retry delay before starting the loop again
+Start-Sleep -s $RetryDelay    
 
-
+            } Until ($RetryCount -ge $RetryLimit)
+        
