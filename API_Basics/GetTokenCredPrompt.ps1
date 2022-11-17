c22d8da9f5d99255e4cd8a7d230e56237431b131
Coan Dillahunty <90055426+cdillahunty-nasuni@users.noreply.github.com>
2022-11-17 17:27:58 -0600

    Add files via upload
    Script that uses PS Credential to Get Creds

---
+++ API_Basics/GetTokenCredPrompt.ps1
@@ -0,0 +1,73 @@
+<# Prompts user for username and password to request an NMC API token and store the output in a Token file that can be used by subsequent scripts.
+Tokens expire after 8 hours. Usernames for AD accounts support both UPN (user@domain.com) and DOMAIN\samaccountname formats.
+Nasuni Native user accounts are also supported. #>
+
+#populate NMC hostname or IP address
+$hostname = "InsertNMChostname"
+
+#Path to token output file
+$tokenFile = "c:\nasuni\token.txt"
+
+#end variables
+
+# Allow untrusted SSL certs
+if ($PSVersionTable.PSEdition -eq 'Core') #PowerShell Core
+{
+	if ($PSDefaultParameterValues.Contains('Invoke-RestMethod:SkipCertificateCheck')) {}
+	else {
+		$PSDefaultParameterValues.Add('Invoke-RestMethod:SkipCertificateCheck', $true)
+	}
+}
+else #other versions of PowerShell
+{if ("TrustAllCertsPolicy" -as [type]) {} else {		
+	
+Add-Type -TypeDefinition @"
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+public class TrustAllCertsPolicy : ICertificatePolicy {
+	public bool CheckValidationResult(
+		ServicePoint srvPoint, X509Certificate certificate,
+		WebRequest request, int certificateProblem) {
+		return true;
+	}
+}
+"@
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object -TypeName TrustAllCertsPolicy
+
+#set the correct TLS Type
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+ } }
+
+#get a credential for authentication
+$cred = Get-Credential
+
+#read the user and pass from the cred - replace single backslash in username with double for JSON
+$username = $cred.UserName.replace('\','\\')
+$password = $cred.GetNetworkCredential().Password
+
+#combine credentials for token request
+$credentials = '{"username":"' + $username + '","password":"' + $password + '"}'
+
+#clear temporaty credential variables
+Clear-Variable username
+Clear-Variable password
+ 
+#construct Uri
+$url="https://"+$hostname+"/api/v1.1/auth/login/"
+
+#build JSON headers
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Accept", 'application/json')
+$headers.Add("Content-Type", 'application/json')
+
+#Use credentials to request and store a session token from NMC for later use
+$result = Invoke-RestMethod -Uri $url -Method Post -Headers $headers -Body $credentials
+$token = $result.token
+$headers.Add("Authorization","Token " + $token)
+
+#write the token to the console to verify script ouput
+write-output $token
+
+#write the updated token to a file
+Set-Content $tokenFile $token
+
