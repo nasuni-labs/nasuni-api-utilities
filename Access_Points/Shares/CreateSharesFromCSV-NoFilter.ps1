4dd0e93c0b6a938cf2bba2d299537034aa7681f3
Coan Dillahunty <90055426+cdillahunty-nasuni@users.noreply.github.com>
2023-03-14 14:43:10 -0500

    Update CreateSharesFromCSV-NoFilter.ps1
    updated Auth to use token file

--- Access_Points/Shares/CreateSharesFromCSV-NoFilter.ps1
+++ Access_Points/Shares/CreateSharesFromCSV-NoFilter.ps1
@@ -8,19 +8,16 @@
 #populate NMC hostname or IP address
 $hostname = "InsertNMChostname"
 
-#username for AD accounts supports both UPN (user@domain.com) and DOMAIN\\samaccountname formats (two backslashes required ). Nasuni Native user accounts are also supported.
-$username = "InsertUsername"
-$password = 'InsertPassword'
+<#Path to the NMC API authentication token file--use GetTokenCredPrompt/GetToken scripts to get a token.
+Tokens expire after 8 hours #>
+$tokenFile = "c:\nasuni\token.txt"
 
 #provide the path to input CSV
 $csvPath = 'c:\import\AllShareProperties.csv'
 
 #end variables
 
-#combine credentials for token request
-$credentials = '{"username":"' + $username + '","password":"' + $password + '"}'
-
-#Request token and build connection headers
+#Build connection headers
 #Allow untrusted SSL certs
 if ($PSVersionTable.PSEdition -eq 'Core') #PowerShell Core
 {
@@ -54,12 +51,8 @@ $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Accept", 'application/json')
 $headers.Add("Content-Type", 'application/json')
 
-#construct Uri
-$url="https://"+$hostname+"/api/v1.1/auth/login/"
- 
-#Use credentials to request and store a session token from NMC for later use
-$result = Invoke-RestMethod -Uri $url -Method Post -Headers $headers -Body $credentials
-$token = $result.token
+#Read the token from a file and add it to the headers for the request
+$token = Get-Content $tokenFile
 $headers.Add("Authorization","Token " + $token)
 
 #Begin share creation
