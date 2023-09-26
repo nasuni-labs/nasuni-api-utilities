339355e6ff219fac8c9d3259b488181d51903239
Coan Dillahunty <90055426+cdillahunty-nasuni@users.noreply.github.com>
2023-09-26 17:29:55 -0400

    Update SetGFLandModeForMultiplePaths.ps1
    use token for authentication

--- Paths/SetGFLandModeForMultiplePaths.ps1
+++ Paths/SetGFLandModeForMultiplePaths.ps1
@@ -1,11 +1,11 @@
-#Enable Global locking and set mode for the specificed paths with a basepath
+#Enable Global locking and set mode for the specified paths with a basepath
   
 #populate NMC hostname and credentials
 $hostname = "host.domain.com"
  
-#username for AD accounts supports both UPN (user@domain.com) and DOMAIN\\samaccountname formats (two backslashes required ). Nasuni Native user accounts are also supported.
-$username = "username"
-$password = 'password'
+<#Path to the NMC API authentication token file--use GetTokenCredPrompt/GetToken scripts to get a token.
+Tokens expire after 8 hours #>
+$tokenFile = "c:\nasuni\token.txt"
  
 #specify the volume
 $volume_guid = "InsertVolumeGuidHere"
@@ -13,16 +13,13 @@ $volume_guid = "InsertVolumeGuidHere"
 #Set the basepath that all specified folders will share
 $basepath = "/paths"
  
-#Set the path thats will be updated within the basepath. The path should start with a "/" and should be separated by a comma
+#Set the path that will be updated within the basepath. The path should start with a "/" and should be separated by a comma
 $FolderPaths = @("/folder1", "/folder2", "/folder2")
  
 #Set the mode - "optimized, advanced, or asynchronous"
 $mode = "optimized"
 
 #end variables
-
-#combine credentials for token request
-$credentials = '{"username":"' + $username + '","password":"' + $password + '"}'
   
 # Allow untrusted SSL certs
 if ($PSVersionTable.PSEdition -eq 'Core') #PowerShell Core
@@ -56,13 +53,9 @@ public class TrustAllCertsPolicy : ICertificatePolicy {
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Accept", 'application/json')
 $headers.Add("Content-Type", 'application/json')
-  
-#construct Uri
-$url="https://"+$hostname+"/api/v1.1/auth/login/"
-   
-#Use credentials to request and store a session token from NMC for later use
-$result = Invoke-RestMethod -Uri $url -Method Post -Headers $headers -Body $credentials
-$token = $result.token
+ 
+#Read the token from a file and add it to the headers for the request
+$token = Get-Content $tokenFile
 $headers.Add("Authorization","Token " + $token)
  
 #Set the URL for the folder update NMC API endpoint
