7e2450ee4d86ce716dcf9cd8dd53368a1ae0526a
Coan Dillahunty <90055426+cdillahunty-nasuni@users.noreply.github.com>
2023-09-26 17:22:34 -0400

    Update ExportPinnedFoldersToCSV.ps1
    use Token for Authentication

--- Paths/ExportPinnedFoldersToCSV.ps1
+++ Paths/ExportPinnedFoldersToCSV.ps1
@@ -3,9 +3,9 @@
 #populate NMC hostname and credentials
 $hostname = "insertHostname"
  
-#username for AD accounts supports both UPN (user@domain.com) and DOMAIN\\samaccountname formats (two backslashes required ). Nasuni Native user accounts are also supported.
-$username = "username"
-$password = 'password'
+<#Path to the NMC API authentication token file--use GetTokenCredPrompt/GetToken scripts to get a token.
+Tokens expire after 8 hours #>
+$tokenFile = "c:\nasuni\token.txt"
 
 #Path for CSV Export
 $reportFile = "c:\export\Pinnedfolders.csv"
@@ -14,9 +14,6 @@ $reportFile = "c:\export\Pinnedfolders.csv"
 $limit = 1000
 
 #end variables
-
-#build credentials
-$credentials = '{"username":"' + $username + '","password":"' + $password + '"}'
 
 #Request token and build connection headers
 # Allow untrusted SSL certs
@@ -51,13 +48,9 @@ public class TrustAllCertsPolicy : ICertificatePolicy {
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Accept", 'application/json')
 $headers.Add("Content-Type", 'application/json')
-
-#construct Uri
-$url="https://"+$hostname+"/api/v1.1/auth/login/"
  
-#Use credentials to request and store a session token from NMC for later use
-$result = Invoke-RestMethod -Uri $url -Method Post -Headers $headers -Body $credentials
-$token = $result.token
+#Read the token from a file and add it to the headers for the request
+$token = Get-Content $tokenFile
 $headers.Add("Authorization","Token " + $token)
 
 #List Pinned Folders NMC API endpoint
