61a9a91e08d296056600e24e9097f5dc4e9b610c
Coan Dillahunty <90055426+cdillahunty-nasuni@users.noreply.github.com>
2023-10-02 13:45:47 -0500

    Update UpdateExport.ps1
    auth with tokenFile

--- Access_Points/Exports/UpdateExport.ps1
+++ Access_Points/Exports/UpdateExport.ps1
@@ -3,9 +3,9 @@
 #populate NMC hostname and credentials
 $hostname = "nmc.coan.com"
   
-#username for AD accounts supports both UPN (user@domain.com) and DOMAIN\\samaccountname formats (two backslashes required). Nasuni Native user accounts are also supported.
-$username = "username"
-$password = 'password'
+<# Path to the NMC API authentication token file--use GetTokenCredPrompt/GetToken scripts to get a token.
+Tokens expire after 8 hours #>
+$tokenFile = "c:\nasuni\token.txt"
 
 #specify Nasuni volume guid and filer serial number
 $filer_serial = "InsertFilerSerial"
@@ -28,10 +28,6 @@ $secOptions = "sys"
 
 
 #end variables
-
-#build credentials
-$credentials = '{"username":"' + $username + '","password":"' + $password + '"}'
-
 
 # Allow untrusted SSL certs
 if ($PSVersionTable.PSEdition -eq 'Core') #PowerShell Core
@@ -66,12 +62,8 @@ $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Accept", 'application/json')
 $headers.Add("Content-Type", 'application/json')
  
-#construct Uri
-$loginUrl="https://"+$hostname+"/api/v1.2/auth/login/"
-  
-#Use credentials to request and store a session token from NMC for later use
-$result = Invoke-RestMethod -Uri $loginUrl -Method Post -Headers $headers -Body $credentials
-$token = $result.token
+#Read the token from a file and add it to the headers for the request
+$token = Get-Content $tokenFile
 $headers.Add("Authorization","Token " + $token)
 
 #Build json body for export update
