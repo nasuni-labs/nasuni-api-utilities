85aeb3762a286f3b77e6963f867b470604d5bfe5
Coan Dillahunty <90055426+cdillahunty-nasuni@users.noreply.github.com>
2023-10-02 13:46:53 -0500

    Update UpdateHostOption.ps1
    auth with tokenFile

--- Access_Points/Exports/UpdateHostOption.ps1
+++ Access_Points/Exports/UpdateHostOption.ps1
@@ -3,9 +3,9 @@
 #populate NMC hostname and credentials
 $hostname = "host.domain.com"
  
-#username for AD accounts supports both UPN (user@domain.com) and DOMAIN\\samaccountname formats (two backslashes required ). Nasuni Native user accounts are also supported.
-$username = "username"
-$password = 'password'
+<# Path to the NMC API authentication token file--use GetTokenCredPrompt/GetToken scripts to get a token.
+Tokens expire after 8 hours #>
+$tokenFile = "c:\nasuni\token.txt"
 
 #specify Nasuni volume guid and filer serial number
 $filer_serial = "InsertFilerSerial"
@@ -31,8 +31,6 @@ $perfMode = "sync"
 $secOptions = "sys"
 
 #end variables
-#build credentials for later use
-$credentials = '{"username":"' + $username + '","password":"' + $password + '"}'
 
 #function for error
 function Failure {
@@ -83,12 +81,8 @@ $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Accept", 'application/json')
 $headers.Add("Content-Type", 'application/json')
  
-#construct Uri
-$url="https://"+$hostname+"/api/v1.1/auth/login/"
-  
-#Use credentials to request and store a session token from NMC for later use
-$result = Invoke-RestMethod -Uri $url -Method Post -Headers $headers -Body $credentials
-$token = $result.token
+#Read the token from a file and add it to the headers for the request
+$token = Get-Content $tokenFile
 $headers.Add("Authorization","Token " + $token)
  
 #Update the host option
