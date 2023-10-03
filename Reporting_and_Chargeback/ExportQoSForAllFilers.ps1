0303ac3092cdc259d9ea6805818e9b38593d8f73
Coan Dillahunty <90055426+cdillahunty-nasuni@users.noreply.github.com>
2023-10-03 10:57:22 -0500

    Update ExportQoSForAllFilers.ps1
    use tokenFile for Auth

--- Reporting_and_Chargeback/ExportQoSForAllFilers.ps1
+++ Reporting_and_Chargeback/ExportQoSForAllFilers.ps1
@@ -3,9 +3,9 @@
 #populate NMC hostname and credentials
 $hostname = "insertNMChostnameHere"
  
-#username for AD accounts supports both UPN (user@domain.com) and DOMAIN\\samaccountname formats (two backslashes required ). Nasuni Native user accounts are also supported.
-$username = "username"
-$password = 'password'
+<# Path to the NMC API authentication token file--use GetTokenCredPrompt/GetToken scripts to get a token.
+Tokens expire after 8 hours #>
+$tokenFile = "c:\nasuni\token.txt"
 
 #Number of Edge Appliances to return in results
 $limit = 500
@@ -14,9 +14,6 @@ $limit = 500
 $reportFile = "c:\QoSExport.csv"
 
 #end variables
-
-#combine credentials for token request
-$credentials = '{"username":"' + $username + '","password":"' + $password + '"}'
 
 #Request token and build connection headers 
 # Allow untrusted SSL certs - remove if valid NMC ssl cert is installed
@@ -52,12 +49,8 @@ $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Accept", 'application/json')
 $headers.Add("Content-Type", 'application/json')
  
-#construct Uri
-$url="https://"+$hostname+"/api/v1.1/auth/login/"
-  
-#Use credentials to request and store a session token from NMC for later use
-$result = Invoke-RestMethod -Uri $url -Method Post -Headers $headers -Body $credentials
-$token = $result.token
+#Read the token from a file and add it to the headers for the request
+$token = Get-Content $tokenFile
 $headers.Add("Authorization","Token " + $token)
 
 #initialize csv output file
