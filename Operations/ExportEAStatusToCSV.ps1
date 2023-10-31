4fb9caefd0ef690c73484ce04f71facd4e12ad6b
Coan Dillahunty <90055426+cdillahunty-nasuni@users.noreply.github.com>
2023-10-31 11:58:34 -0500

    Update ExportEAStatusToCSV.ps1

--- Operations/ExportEAStatusToCSV.ps1
+++ Operations/ExportEAStatusToCSV.ps1
@@ -1,5 +1,5 @@
-#Export All Edge Appliance Info to CSV
-#Uses the list Edge Appliances NMC API endpoint - http://docs.api.nasuni.com/nmc/api/1.1.0/index.html#list-filers
+<#Export All Edge Appliance Info to CSV. Uses the list Edge Appliances NMC API endpoint
+https://docs.api.nasuni.com/api/nmc/v100/reference/tag/Filers/paths/~1filers~1/get/ #>
 
 #populate NMC hostname and credentials
 $hostname = "host.domain.com"
@@ -53,11 +53,11 @@ $token = Get-Content $tokenFile
 $headers.Add("Authorization","Token " + $token)
   
 #Initialize CSV output file
-$csvHeader = "Description,SerialNumber,GUID,build,cpuCores,cpuModel,cpuFrequency,cpuSockets,Memory,ManagementState,Offline,OsVersion,Uptime,UpdatesAvailable,CurrentVersion,NewVersion,PlatformName,cacheSize,cacheUsed,cacheDirty,cacheFree,cachePercentUsed"
+$csvHeader = "Description,SerialNumber,GUID,build,cpuCores,cpuModel,cpuFrequency,cpuSockets,Memory,ManagementState,Offline,OsVersion,Uptime,UpdatesAvailable,CurrentVersion,NewVersion,PlatformName,cacheSize,cacheUsed,cacheDirty,cacheFree,cachePercentUsed,Hostname,DefaultGateway,IpAddresses,DnsServers,SearchDomains,RemoteSupportConnected,RemoteSupportRunning,RemoteSupportEnabled,RemoteSupportTimeout"
 Out-File -FilePath $reportFile -InputObject $csvHeader -Encoding UTF8
 
 #List filer info
-$FilerInfoUrl="https://"+$hostname+"/api/v1.1/filers/?limit="+$limit+"&offset=0"
+$FilerInfoUrl="https://"+$hostname+"/api/v1.2/filers/?limit="+$limit+"&offset=0"
 $GetFilerInfo = Invoke-RestMethod -Uri $FilerInfoUrl -Method Get -Headers $headers
    
 foreach($i in 0..($GetFilerInfo.items.Count-1)){
@@ -75,23 +75,32 @@ foreach($i in 0..($GetFilerInfo.items.Count-1)){
         $Offline = $GetFilerInfo.items[$i].status.offline
         $OsVersion = $GetFilerInfo.items[$i].status.osversion
         $Uptime = $GetFilerInfo.items[$i].status.uptime
-        $UpdatesAvailable = $GetFilerInfo.items[$i].status.updates_available
-        $CurrentVersion = $GetFilerInfo.items[$i].status.current_version
-        $NewVersion = $GetFilerInfo.items[$i].status.new_version
+        $UpdatesAvailable = $GetFilerInfo.items[$i].status.updates.updates_available
+        $CurrentVersion = $GetFilerInfo.items[$i].status.updates.current_version
+        $NewVersion = $GetFilerInfo.items[$i].status.updates.new_version
         $PlatformName = $GetFilerInfo.items[$i].status.platform.platform_name
         $cacheSize = $GetFilerInfo.items[$i].status.platform.cache_status.size
         $cacheUsed = $GetFilerInfo.items[$i].status.platform.cache_status.used
         $cacheDirty = $GetFilerInfo.items[$i].status.platform.cache_status.dirty
         $cacheFree = $GetFilerInfo.items[$i].status.platform.cache_status.free
         $cachePercentUsed = $GetFilerInfo.items[$i].status.platform.cache_status.percent_used
+        $hostname = $GetFilerInfo.items[$i].settings.network_settings.hostname
+        $defaultGateway = $GetFilerInfo.items[$i].settings.network_settings.default_gateway
+        $ipAddresses = $GetFilerInfo.items[$i].settings.network_settings.ip_addresses | Join-String -Separator ';'
+        $dnsServers = $GetFilerInfo.items[$i].settings.network_settings.dns_servers | Join-String -Separator ';'
+        $searchDomains = $GetFilerInfo.items[$i].settings.network_settings.search_domains | Join-String -Separator ';'
+        $remoteSupportConnected = $GetFilerInfo.items[$i].settings.remote_support.connected
+        $remoteSupportRunning = $GetFilerInfo.items[$i].settings.remote_support.running
+        $remoteSupportEnabled = $GetFilerInfo.items[$i].settings.remote_support.enabled
+        $remoteSupportTimeout = $GetFilerInfo.items[$i].settings.remote_support.timeout
       
         #write the output to a variable
-        $datastring = "$Description,$SerialNumber,$GUID,$build,$cpuCores,$cpuModel,$cpuFrequency,$cpuSockets,$Memory,$ManagementState,$Offline,$OsVersion,$Uptime,$UpdatesAvailable,$CurrentVersion,$NewVersion,$PlatformName,$cacheSize,$cacheUsed,$cacheDirty,$cacheFree,$cachePercentUsed"
+        $datastring = "$Description,$SerialNumber,$GUID,$build,$cpuCores,$cpuModel,$cpuFrequency,$cpuSockets,$Memory,$ManagementState,$Offline,$OsVersion,$Uptime,$UpdatesAvailable,$CurrentVersion,$NewVersion,$PlatformName,$cacheSize,$cacheUsed,$cacheDirty,$cacheFree,$cachePercentUsed,$hostname,$defaultGateway,$ipAddresses,$dnsServers,$searchDomains,$remoteSupportConnected,$remoteSupportRunning,$remoteSupportEnabled,$remoteSupportTimeout"
         #write the output to a file
         Out-File -FilePath $reportFile -InputObject $datastring -Encoding UTF8 -append
  
         #clear variables for next loop
-        $ClearVar = ("Description", "SerialNumber", "GUID", "build", "cpuCores", "cpuModel", "cpuFrequency", "cpuSockets", "Memory", "ManagementState", "Offline", "OsVersion", "Uptime", "UpdatesAvailable", "CurrentVersion", "NewVersion", "PlatformName", "cacheSize", "cacheUsed", "cacheDirty", "CacheFree", "CachePercentUsed")
+        $ClearVar = ("Description", "SerialNumber", "GUID", "build", "cpuCores", "cpuModel", "cpuFrequency", "cpuSockets", "Memory", "ManagementState", "Offline", "OsVersion", "Uptime", "UpdatesAvailable", "CurrentVersion", "NewVersion", "PlatformName", "cacheSize", "cacheUsed", "cacheDirty", "CacheFree", "CachePercentUsed", "hostname", "defaultGateway", "ipAddresses","dnsServers","searchDomains","remoteSupportConnected","remoteSupportRunning","remoteSupportEnabled","remoteSupportTimeout")
         foreach ($Item in $ClearVar) {
         Get-Variable $Item | Remove-Variable
         }
