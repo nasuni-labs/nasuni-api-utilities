4be85c1f1e1ab037d7bcf53f72feb59e24f9615e
Coan Dillahunty <90055426+cdillahunty-nasuni@users.noreply.github.com>
2024-04-12 09:10:30 -0500

    Update CreateSharesFromCSV-WithFilter.ps1
    Use defaults if a share has empty CSV properties.

--- Access_Points/Shares/CreateSharesFromCSV-WithFilter.ps1
+++ Access_Points/Shares/CreateSharesFromCSV-WithFilter.ps1
@@ -18,7 +18,7 @@ $csvPath = 'c:\import\AllShareProperties.csv'
 #end variables
 
 #convert volumeguid input to an array
-#if no volume, create a empty array
+#if no volume create a empty array
 if ($null -eq $matchVolumeGuid) {$matchVolumeGuidArray = @()}
 else { #split the volume to create an array
 $matchVolumeGuidArray = $matchVolumeGuid.Split(",")
@@ -89,88 +89,6 @@ ForEach ($share in $shares) {
     $comment = $($share.comment)
     $readonly = $($share.readonly).ToLower()
     $browseable = $($share.browseable).ToLower()
-    if (!$share.authAuthall) {$authAuthall = "true"} else {$authAuthall = $($share.authAuthall).ToLower()}
-    if (!$share.authRo_users) {Clear-Variable authRo_users -ErrorAction SilentlyContinue} else {$authRo_users = "'"+$($share.authRo_users)+"'" -replace '\\','\\' -replace ';',''',''' -replace "'",'"'}
-    if (!$share.authRw_users) {Clear-Variable authRw_users -ErrorAction SilentlyContinue} else {$authRw_users = "'"+$($share.authRw_users)+"'" -replace '\\','\\' -replace ';',''',''' -replace "'",'"'}
-    if (!$share.authDeny_users) {Clear-Variable authDeny_users -ErrorAction SilentlyContinue} else {$authDeny_users = "'"+$($share.authDeny_users)+"'" -replace '\\','\\' -replace ';',''',''' -replace "'",'"'}
-    if (!$share.authRo_groups) {Clear-Variable authRo_groups -ErrorAction SilentlyContinue} else {$authRo_groups = "'"+$($share.authRo_groups)+"'" -replace '\\','\\' -replace ';',''',''' -replace "'",'"'}
-    if (!$share.authRw_groups) {Clear-Variable authRw_groups -ErrorAction SilentlyContinue} else {$authRw_groups = "'"+$($share.authRw_groups)+"'" -replace '\\','\\' -replace ';',''',''' -replace "'",'"'}
-    if (!$share.authDeny_groups) {Clear-Variable authDeny_groups -ErrorAction SilentlyContinue} else {$authDeny_groups = "'"+$($share.authDeny_groups)+"'" -replace '\\','\\' -replace ';',''',''' -replace "'",'"'}
-    $hosts_allow = $($share.hosts_allow) -replace ";"," "
-    if (!$share.hide_unreadable){$hide_unreadable = "true"} else {$hide_unreadable = $($share.hide_unreadable).ToLower()}
-    if (!$share.enable_previous_vers){$enable_previous_vers = "false"} else {$enable_previous_vers = $($share.enable_previous_vers).ToLower()}
-    if (!$share.case_sensitive){$case_sensitive = "false"} else {$case_sensitive = $($share.case_sensitive).ToLower()}
-    if (!$share.enable_snapshot_dirs){$enable_snapshot_dirs = "false"} else {$enable_snapshot_dirs = $($share.enable_snapshot_dirs).ToLower()}
-    if (!$share.homedir_support){$homedir_support = "0"} else {$homedir_support = $($share.homedir_support)}
-    if (!$share.mobile){$mobile = "false"} else {$mobile = $($share.mobile).ToLower()}
-    if (!$share.browser_access) {$browser_access = "false"} else {$browser_access = $($share.browser_access).ToLower()}
-    if (!$share.aio_enabled) {$aio_enabled = "true"} else {$aio_enabled = $($share.aio_enabled).ToLower()}
-    $veto_files = $($share.veto_files) -replace ";", "\r\n"
-#creates shares from a CSV with some hard-coded presets for shares
-#CSV column order - shareid(skipped for during share creation),Volume_GUID,filer_serial_number,filer_name,share_name,path,comment,readonly,browseable,authAuthAll,authRo_users,authRw_users,authDeny_users,authRo_groups,authRw_groups,authDeny_groups,hosts_allow,hide_unreadable,enable_previous_vers,case_sensitive,enable_snapshot_dirs,homedir_support,mobile,browser_access,aio_enabled,veto_files,fruit_enabled,smb_encrypt,shared_links_enabled,link_force_password,link_allow_rw,external_share_url,link_expire_limit,link_auth.authall,link_authAllow_groups_ro,link_auth.allow_groups_rw,link_auth.deny_groups,link_auth.allow_users_ro,link_auth.allow_users_rw,link_auth.deny_users
-
-#populate NMC hostname or IP address
-$hostname = "InsertNMChostname"
-
-<#Path to the NMC API authentication token file--use GetTokenCredPrompt/GetToken scripts to get a token.
-Tokens expire after 8 hours #>
-$tokenFile = "c:\nasuni\token.txt"
-
-#provide the path to input CSV
-$csvPath = 'c:\import\AllShareProperties.csv'
-
-#end variables
-
-#Build connection headers
-#Allow untrusted SSL certs
-if ($PSVersionTable.PSEdition -eq 'Core') #PowerShell Core
-{
-	if ($PSDefaultParameterValues.Contains('Invoke-RestMethod:SkipCertificateCheck')) {}
-	else {
-		$PSDefaultParameterValues.Add('Invoke-RestMethod:SkipCertificateCheck', $true)
-	}
-}
-else #other versions of PowerShell
-{if ("TrustAllCertsPolicy" -as [type]) {} else {		
-	
-Add-Type -TypeDefinition @"
-using System.Net;
-using System.Security.Cryptography.X509Certificates;
-public class TrustAllCertsPolicy : ICertificatePolicy {
-	public bool CheckValidationResult(
-		ServicePoint srvPoint, X509Certificate certificate,
-		WebRequest request, int certificateProblem) {
-		return true;
-	}
-}
-"@
-[System.Net.ServicePointManager]::CertificatePolicy = New-Object -TypeName TrustAllCertsPolicy
-
-#set the correct TLS Type
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
- } }
-
-#build JSON headers
-$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$headers.Add("Accept", 'application/json')
-$headers.Add("Content-Type", 'application/json')
-
-#Read the token from a file and add it to the headers for the request
-$token = Get-Content $tokenFile
-$headers.Add("Authorization","Token " + $token)
-
-#Begin share creation
-#read the contents of the CSV into variables
-$shares = Get-Content $csvPath | Select-Object -Skip 1 | ConvertFrom-Csv -header "shareid","Volume_GUID","Volume_Name","filer_serial_number","filer_name","share_name","path","comment","readonly","browseable","authAuthall","authRo_users","authRw_users","authDeny_users","authRo_groups","authRw_groups","authDeny_groups","hosts_allow","hide_unreadable","enable_previous_vers","case_sensitive","enable_snapshot_dirs","homedir_support","mobile","browser_access","aio_enabled","veto_files","fruit_enabled","smb_encrypt","shared_links_enabled","link_force_password","link_allow_rw","external_share_url","link_expire_limit","link_authAuthall","link_authAllow_groups_ro","link_authAllow_groups_rw","link_authDeny_groups","link_authAllow_users_ro","link_authAllow_users_rw","link_authDeny_users"
-
-ForEach ($share in $shares) {
-    $volume_guid = $($share.Volume_Guid)
-    $filer_serial_number = $($share.filer_serial_number)
-    $share_name = $($share.share_name)
-    $path = $($share.path) -replace '\\','\\'
-    $comment = $($share.comment)
-    if (!$share.readonly){$readonly = "false"} else {$readonly = $($share.readonly).ToLower()}
-    if (!$share.browseable) {$browseable = "true"} else {$browseable = $($share.browseable).ToLower()}
     if (!$share.authAuthall) {$authAuthall = "true"} else {$authAuthall = $($share.authAuthall).ToLower()}
     if (!$share.authRo_users) {Clear-Variable authRo_users -ErrorAction SilentlyContinue} else {$authRo_users = "'"+$($share.authRo_users)+"'" -replace '\\','\\' -replace ';',''',''' -replace "'",'"'}
     if (!$share.authRw_users) {Clear-Variable authRw_users -ErrorAction SilentlyContinue} else {$authRw_users = "'"+$($share.authRw_users)+"'" -replace '\\','\\' -replace ';',''',''' -replace "'",'"'}
